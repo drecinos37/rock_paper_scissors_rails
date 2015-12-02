506d401c9b2fa23217446ab4888b18f1757c82e1
drecinos37 <drecinos@chicagobooth.edu>
2015-12-01 21:57:01 -0600

    Forgot to save

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,9 +7,40 @@ class GameController < ApplicationController
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = ["rock", "paper", "scissors"].sample
 
-    @result = "Replace this string with the correct value."
+puts "Your move: rock, paper, or scissors?"
+
+@user_move = gets.chomp.downcase
+@computer_move = ["rock","paper","scissors"].sample
+
+puts "Computer chose #{@computer_move}"
+puts "You chose #{move}"
+
+
+if @user_move == @computer_move
+    puts "It's a tie!"
+  elsif @user_move == "rock" && @computer_move == "paper"
+ result=1
+    elsif @user_move == "paper" && @computer_move == "scissors"
+   result=2
+    elsif @user_move == "scissors" && @computer_move == "rock"
+    result=2
+  elsif @user_move == "rock" && @computer_move == "scissors"
+    result=1
+  elsif @user_move == "paper" && @computer_move == "rock"
+    result=1
+  elsif @user_move == "scissors" && @computer_move == "paper"
+    result=1
+      # user rock, computer paper
+end
+
+
+if result =1
+    x = "You won"
+else x = "You lost"
+
+    @result = "#{x}"
 
     # ===============================================================
     # Your code goes above.
